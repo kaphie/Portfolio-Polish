7393f7d1dad82661928daecbc60facc50dc2b3b7
kaphie <bkafrika144@gmail.com>
2020-06-23 11:16:44 +0300

    adding images

---
+++ css/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".clickable1").click(function(){
+        $(".img11").toggle();
+        $(".textclickable1").toggle();
+    });
+    $(".clickable2").click(function(){
+        $(".img22").toggle();
+        $(".textclickable2").toggle();
+    });
+    $(".clickable3").click(function(){
+        $(".img33").toggle();
+        $(".textclickable3").toggle();
+    });
+});
